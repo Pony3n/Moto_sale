3743fdece31cd01c24dda8deddd19b3084fb420b
druce <alisfik@mail.ru>
2024-02-07 19:10:38 +0300

    Added Detail page for lots.

--- moto_seller/motorcycles/urls.py
+++ moto_seller/motorcycles/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import show_main, show_about, show_contact
+from .views import show_main, show_about, show_contact, MotorcycleDetailView
 
 urlpatterns = [
     path('', show_main, name="show_main"),
     path('about/', show_about, name="show_about"),
     path('contact/', show_contact, name="show_contact"),
+    path('<int:pk>/', MotorcycleDetailView.as_view(), name="motorcycle_detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
