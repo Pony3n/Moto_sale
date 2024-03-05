61c9a1f08b4285b6998c66743dd33d6ef193fd9c
druce <alisfik@mail.ru>
2024-03-05 13:34:10 +0300

    Added Login page\view and logout view Also some styles for User's profile page

--- moto_seller/moto_user/forms.py
+++ moto_seller/moto_user/forms.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate
 from django import forms
+
 from .models import MotoUser
 
 
@@ -24,3 +26,27 @@ class MotoUserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
         return password2
+
+
+class MotoUserLoginForm(forms.Form):
+    login = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(MotoUserLoginForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        login = self.cleaned_data.get('login')
+        password = self.cleaned_data.get('password')
+
+        user = authenticate(request=self.request, username=login, password=password)
+
+        if user is None or not user.is_active:
+            raise forms.ValidationError("Неверные учетные данные. Пожалуйста, попробуйте снова.")
+
+        self.user = user
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user if hasattr(self, 'user') else None
